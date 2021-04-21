e4582724aff04e453e5f199bafea536cd56557f2
TRMO <tromortensen@hotmail.com>
2021-04-21 21:00:19 +0200

    Initial commit. Local db seems to work.

---
+++ LocalDB/LocalAuthorContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace LocalDB
+{
+    public class LocalAuthorContext : DbContext
+    {
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=LocalAuthor;Username=postgres;Password=TroelsDatabase",
+                options => options.UseAdminDatabase("LocalAuthor"));
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+﻿LocalDB:
+
+I had to manually create a database, outside of EFC. I called it "LocalAuthor". I used datagrip to create it.
